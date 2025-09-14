7978341c01939296b7ea29c99b5dbca075cd65d7
Karthik <53650724+vkartk@users.noreply.github.com>
2025-09-14 17:36:24 +0530

    feat: add Register Method

---
+++ backend/src/TheBlueSky.Auth/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TheBlueSky.Auth.DTOs.Requests;
+using TheBlueSky.Auth.DTOs.Responses;
+using TheBlueSky.Auth.Services;
+
+namespace TheBlueSky.Auth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public AuthController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] CreateUserRequest request)
+        {
+            var result = await _userService.RegisterUser(request);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(new
+                {
+                    Status = "Error",
+                    result.Errors
+                });
+            }
+
+            return Ok(new AuthResponse
+            { 
+                Status = "Success",
+                Message = "User created successfully!" 
+            });
+
+        }
+
+
+    }
+}
---
+++ backend/src/TheBlueSky.Auth/DTOs/Responses/AuthResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace TheBlueSky.Auth.DTOs.Responses
+{
+    public class AuthResponse
+    {
+        public string Status { get; set; }
+        public string? Message { get; set; }
+
+    }
+}
