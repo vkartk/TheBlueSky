91d022f4d644c465b8e092850e6b87a573005289
Karthik <53650724+vkartk@users.noreply.github.com>
2025-09-15 22:59:59 +0530

    feat: add CountryService

---
+++ backend/src/TheBlueSky.Flights/Services/CountryService.cs
@@ -0,0 +1,25 @@
+﻿using TheBlueSky.Flights.Models;
+using TheBlueSky.Flights.Repositories;
+
+namespace TheBlueSky.Flights.Services
+{
+    public class CountryService : ICountryService
+    {
+        private readonly ICountryRepository _countryRepository;
+
+        public CountryService(ICountryRepository countryRepository)
+        {
+            _countryRepository = countryRepository;
+        }
+
+        public async Task<IEnumerable<Country>> GetAllCountries()
+        {
+            return await _countryRepository.GetAllCountriesAsync();
+        }
+
+        public async Task<Country> GetCountryById(string countryId)
+        {
+            return await _countryRepository.GetCountryByIdAsync(countryId);
+        }
+    }
+}
---
+++ backend/src/TheBlueSky.Flights/Services/ICountryService.cs
@@ -0,0 +1,11 @@
+﻿using TheBlueSky.Flights.Models;
+
+namespace TheBlueSky.Flights.Services
+{
+    public interface ICountryService
+    {
+        Task<IEnumerable<Country>> GetAllCountries();
+        Task<Country> GetCountryById(string countryId);
+
+    }
+}
