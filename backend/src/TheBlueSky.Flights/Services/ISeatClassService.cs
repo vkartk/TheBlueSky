19f4dcd168dcf6714778774caf5c5b8fc7c8fe04
Karthik <53650724+vkartk@users.noreply.github.com>
2025-09-15 23:45:38 +0530

    feat: add SeatClassService

---
+++ backend/src/TheBlueSky.Flights/Services/ISeatClassService.cs
@@ -0,0 +1,14 @@
+﻿using TheBlueSky.Flights.Models;
+
+namespace TheBlueSky.Flights.Services
+{
+    public interface ISeatClassService
+    {
+        Task<IEnumerable<SeatClass>> GetAllSeatClassesAsync();
+        Task<SeatClass?> GetSeatClassByIdAsync(int id);
+        Task<SeatClass> CreateSeatClassAsync(SeatClass seatClass);
+        Task<bool> UpdateSeatClassAsync(int id, SeatClass seatClass);
+        Task<bool> DeleteSeatClassAsync(int id);
+
+    }
+}
---
+++ backend/src/TheBlueSky.Flights/Services/SeatClassService.cs
@@ -0,0 +1,60 @@
+﻿using TheBlueSky.Flights.Models;
+using TheBlueSky.Flights.Repositories;
+
+namespace TheBlueSky.Flights.Services
+{
+    public class SeatClassService : ISeatClassService
+    {
+        private readonly ISeatClassRepository _seatClassRepository;
+
+        public SeatClassService(ISeatClassRepository seatClassRepository)
+        {
+            _seatClassRepository = seatClassRepository;
+        }
+
+        public async Task<IEnumerable<SeatClass>> GetAllSeatClassesAsync()
+        {
+            return await _seatClassRepository.GetAllSeatClassesAsync();
+        }
+
+        public async Task<SeatClass?> GetSeatClassByIdAsync(int id)
+        {
+            return await _seatClassRepository.GetSeatClassByIdAsync(id);
+        }
+
+        public async Task<SeatClass> CreateSeatClassAsync(SeatClass seatClass)
+        {
+            await _seatClassRepository.AddSeatClassAsync(seatClass);
+            return seatClass;
+        }
+
+        public async Task<bool> UpdateSeatClassAsync(int id, SeatClass seatClass)
+        {
+            if (id != seatClass.SeatClassId)
+            {
+                return false;
+            }
+
+            var exists = await _seatClassRepository.SeatClassExists(id);
+            if (!exists)
+            {
+                return false;
+            }
+
+            await _seatClassRepository.UpdateSeatClassAsync(seatClass);
+            return true;
+        }
+
+        public async Task<bool> DeleteSeatClassAsync(int id)
+        {
+            var exists = await _seatClassRepository.SeatClassExists(id);
+            if (!exists)
+            {
+                return false;
+            }
+
+            await _seatClassRepository.DeleteSeatClassAsync(id);
+            return true;
+        }
+    }
+}
