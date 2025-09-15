7e46fb2bfc08430f93370e2cd4b73d17efa2cf8e
Karthik <53650724+vkartk@users.noreply.github.com>
2025-09-15 22:59:38 +0530

    feat: add CountryRepository

---
+++ backend/src/TheBlueSky.Flights/Repositories/CountryRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TheBlueSky.Flights.Models;
+
+namespace TheBlueSky.Flights.Repositories
+{
+    public class CountryRepository
+    {
+        private readonly FlightsDbContext _context;
+
+        public CountryRepository(FlightsDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Country>> GetAllCountriesAsync()
+        {
+            return await _context.Countries.ToListAsync();
+        }
+
+        public async Task<Country> GetCountryByIdAsync(string countryId)
+        {
+            return await _context.Countries.FindAsync(countryId);
+        }
+
+
+
+    }
+}
---
+++ backend/src/TheBlueSky.Flights/Repositories/ICountryRepository.cs
@@ -0,0 +1,11 @@
+﻿using TheBlueSky.Flights.Models;
+
+namespace TheBlueSky.Flights.Repositories
+{
+    public interface ICountryRepository
+    {
+        Task<IEnumerable<Country>> GetAllCountriesAsync();
+        Task<Country> GetCountryByIdAsync(string countryId);
+
+    }
+}
