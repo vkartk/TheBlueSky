9346824ad9a91103d457daba7eace6d71b9ea743
Karthik <53650724+vkartk@users.noreply.github.com>
2025-09-15 23:00:09 +0530

    feat: add CountriesController

---
+++ backend/src/TheBlueSky.Flights/Controllers/CountriesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TheBlueSky.Flights.Models;
+using TheBlueSky.Flights.Services;
+
+namespace TheBlueSky.Flights.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        private readonly ICountryService _countryService;
+
+        public CountriesController(ICountryService countryService)
+        {
+            _countryService = countryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Country>>> GetAllCountries()
+        {
+            var countries = await _countryService.GetAllCountries();
+            return Ok(countries);
+        }
+
+        [HttpGet("{countryId}")]
+        public async Task<ActionResult<Country>> GetCountryById(string countryId)
+        {
+            var country = await _countryService.GetCountryById(countryId);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return Ok(country);
+        }
+
+    }
+}
