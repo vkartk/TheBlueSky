253e85fe2f34e55de5c4be0549d6e1dcfad96d55
Karthik <53650724+vkartk@users.noreply.github.com>
2025-09-15 21:54:12 +0530

    feat: add SeatClass Model

---
+++ backend/src/TheBlueSky.Flights/Models/SeatClass.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheBlueSky.Flights.Models
+{
+    public class SeatClass
+    {
+        public int SeatClassId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string ClassName { get; set; } = null!;
+
+        [StringLength(255)]
+        public string? ClassDescription { get; set; }
+
+        [Required]
+        public int PriorityOrder { get; set; }
+        public ICollection<AircraftSeat> Seats { get; set; } = new List<AircraftSeat>();
+
+    }
+}
