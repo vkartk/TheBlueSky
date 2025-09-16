b436c166ef55bf2d0fdc4e6edadfc4bfd75898b2
Karthik <53650724+vkartk@users.noreply.github.com>
2025-09-16 10:03:25 +0530

    feat: add AircraftSeatRepository

---
+++ backend/src/TheBlueSky.Flights/Repositories/AircraftSeatRepository.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TheBlueSky.Flights.Models;
+
+namespace TheBlueSky.Flights.Repositories
+{
+    public class AircraftSeatRepository : IAircraftSeatRepository
+    {
+        private readonly FlightsDbContext _context;
+
+        public AircraftSeatRepository(FlightsDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<AircraftSeat>> GetAllAircraftSeatsAsync()
+        {
+            return await _context.AircraftSeats.ToListAsync();
+        }
+
+        public async Task<AircraftSeat?> GetAircraftSeatByIdAsync(int id)
+        {
+            return await _context.AircraftSeats.FindAsync(id);
+        }
+
+        public async Task<AircraftSeat> AddAircraftSeatAsync(AircraftSeat aircraftSeat)
+        {
+            _context.AircraftSeats.Add(aircraftSeat);
+            await _context.SaveChangesAsync();
+            return aircraftSeat;
+        }
+
+        public async Task<bool> UpdateAircraftSeatAsync(AircraftSeat aircraftSeat)
+        {
+            _context.Entry(aircraftSeat).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await ExistsAsync(aircraftSeat.AircraftSeatId))
+                {
+                    return false;
+                }
+                throw;
+            }
+        }
+
+        public async Task<bool> DeleteAircraftSeatAsync(int id)
+        {
+            var aircraftSeat = await _context.AircraftSeats.FindAsync(id);
+            if (aircraftSeat == null)
+            {
+                return false;
+            }
+
+            _context.AircraftSeats.Remove(aircraftSeat);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> ExistsAsync(int id)
+        {
+            return await _context.AircraftSeats.AnyAsync(e => e.AircraftSeatId == id);
+        }
+
+    }
+}
---
+++ backend/src/TheBlueSky.Flights/Repositories/IAircraftSeatRepository.cs
@@ -0,0 +1,15 @@
+﻿using TheBlueSky.Flights.Models;
+
+namespace TheBlueSky.Flights.Repositories
+{
+    public interface IAircraftSeatRepository
+    {
+        Task<IEnumerable<AircraftSeat>> GetAllAircraftSeatsAsync();
+        Task<AircraftSeat?> GetAircraftSeatByIdAsync(int id);
+        Task<AircraftSeat> AddAircraftSeatAsync(AircraftSeat aircraftSeat);
+        Task<bool> UpdateAircraftSeatAsync(AircraftSeat aircraftSeat);
+        Task<bool> DeleteAircraftSeatAsync(int id);
+        Task<bool> ExistsAsync(int id);
+
+    }
+}
