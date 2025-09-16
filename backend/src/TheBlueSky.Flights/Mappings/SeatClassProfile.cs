51895c5036165a365bb5b4f90b953a89b87a67ec
Karthik <53650724+vkartk@users.noreply.github.com>
2025-09-16 09:21:50 +0530

    feat: add SeatClassProfile

---
+++ backend/src/TheBlueSky.Flights/Mappings/SeatClassProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using TheBlueSky.Flights.DTOs.Requests.SeatClass;
+using TheBlueSky.Flights.Models;
+
+namespace TheBlueSky.Flights.Mappings
+{
+    public class SeatClassProfile : Profile
+    {
+        public SeatClassProfile()
+        {
+            CreateMap<CreateSeatClassRequest, SeatClass>();
+            CreateMap<UpdateSeatClassRequest, SeatClass>();
+            CreateMap<SeatClass, SeatClassDto>();
+        }
+
+    }
+}
