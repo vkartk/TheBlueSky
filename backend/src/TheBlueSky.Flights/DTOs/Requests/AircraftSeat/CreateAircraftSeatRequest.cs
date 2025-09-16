1ba1c3c1abf9ab25781101eaa08374a32c6dd521
Karthik <53650724+vkartk@users.noreply.github.com>
2025-09-16 10:00:47 +0530

    feat: add AircraftSeat DTOs

---
+++ backend/src/TheBlueSky.Flights/DTOs/Requests/AircraftSeat/CreateAircraftSeatRequest.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheBlueSky.Flights.DTOs.Requests.AircraftSeat
+{
+    public record CreateAircraftSeatRequest(
+
+        [Required] 
+        int AircraftId,
+
+        [Required]
+        int SeatClassId,
+
+        [Required]
+        [StringLength(10)] 
+        string SeatNumber,
+
+        [Required]
+        [StringLength(10)] 
+        string SeatPosition,
+
+        [Required] 
+        decimal AdditionalFare,
+
+        [Required] 
+        int SeatRow,
+
+        [Required] 
+        int SeatColumn
+    );
+
+}
---
+++ backend/src/TheBlueSky.Flights/DTOs/Responses/AircraftSeat/AircraftSeatResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace TheBlueSky.Flights.DTOs.Responses.AircraftSeat
+{
+    public record AircraftSeatResponse(
+
+        int AircraftSeatId,
+
+        int AircraftId,
+
+        int SeatClassId,
+
+        string SeatNumber,
+
+        string SeatPosition,
+
+        decimal AdditionalFare,
+
+        int SeatRow,
+
+        int SeatColumn,
+
+        bool IsActive
+    );
+
+}
