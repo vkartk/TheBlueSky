e49b7320ef706622cba850355e15e56617ced69a
Karthik <53650724+vkartk@users.noreply.github.com>
2025-09-16 20:52:22 +0530

    feat: add Passenger DTOs

---
+++ backend/src/TheBlueSky.Bookings/DTOs/Requests/Passenger/UpdatePassengerRequest.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheBlueSky.Bookings.DTOs.Requests.Passenger
+{
+    public record UpdatePassengerRequest(
+
+        [Required]
+        int PassengerId,
+
+        [Required]
+        int ManagedByUserId,
+
+        [Required]
+        [MaxLength(100)]
+        string FirstName,
+
+        [Required]
+        [MaxLength(100)]
+        string LastName,
+
+        [Required]
+        [DataType(DataType.Date)]
+        DateTime DateOfBirth,
+
+        [MaxLength(16)]
+        string? Gender,
+
+        [MaxLength(32)]
+        string? PassportNumber,
+
+        [MaxLength(2)]
+        string? NationalityCountryId,
+
+        [MaxLength(64)]
+        string? RelationshipToManager,
+
+        [Required]
+        bool IsActive
+    );
+
+}
---
+++ backend/src/TheBlueSky.Bookings/DTOs/Responses/Passenger/PassengerResponse.cs
@@ -0,0 +1,28 @@
+﻿namespace TheBlueSky.Bookings.DTOs.Responses.Passenger
+{
+    public record PassengerResponse(
+
+        int PassengerId,
+
+        int ManagedByUserId,
+
+        string FirstName,
+
+        string LastName,
+
+        DateTime DateOfBirth,
+
+        string? Gender,
+
+        string? PassportNumber,
+
+        string? NationalityCountryId,
+
+        string? RelationshipToManager,
+
+        DateTime CreatedDate,
+
+        bool IsActive
+    );
+
+}
