a1b2a124920208ee6d2991bee9859e37e95575c0
Karthik <53650724+vkartk@users.noreply.github.com>
2025-09-25 10:05:42 +0530

    feat: add login Page

--- frontend/TheBlueSky.App/src/App.tsx
+++ frontend/TheBlueSky.App/src/App.tsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Route, Routes } from 'react-router'
+import LoginPage from './pages/auth/login'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Routes>
         <Route path="/" element="Home" />
+        <Route path='/login' element={<LoginPage/>} />
       </Routes>
     </>
   )
