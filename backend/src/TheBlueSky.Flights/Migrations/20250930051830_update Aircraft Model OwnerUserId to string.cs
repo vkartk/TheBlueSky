bc9af3827159b9cf876fbb9849da9bd2c0054aca
Karthik <53650724+vkartk@users.noreply.github.com>
2025-09-30 10:49:23 +0530

    fix(flights): Add Aircraft Model OwnerUserId to string Migration

---
+++ backend/src/TheBlueSky.Flights/Migrations/20250930051830_update Aircraft Model OwnerUserId to string.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheBlueSky.Flights.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateAircraftModelOwnerUserIdtostring : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OwnerUserId",
+                table: "Aircrafts",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "OwnerUserId",
+                table: "Aircrafts",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
