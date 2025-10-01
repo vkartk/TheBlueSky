599ceeed1ae2207bb45c3ec31e8310222f8e44f1
Karthik <53650724+vkartk@users.noreply.github.com>
2025-10-01 19:56:53 +0530

    feat: add getWithSeats to Aircraft Service

--- frontend/TheBlueSky.App/src/services/flights/aircraftService.ts
+++ frontend/TheBlueSky.App/src/services/flights/aircraftService.ts
@@ -1,10 +1,15 @@
-import type { Aircraft, NewAircraft } from '@/types/aircraft';
+import type { Aircraft, AircraftWithSeats, NewAircraft } from '@/types/aircraft';
 import { flightsApiClient } from './flightsApiClient';
 
 const API_PATH = '/aircraft';
 
 const getAll = async (): Promise<Aircraft[]> => {
   const response = await flightsApiClient.get<Aircraft[]>(API_PATH);
+  return response.data;
+};
+
+const getWithSeats = async (id: number): Promise<AircraftWithSeats> => {
+  const response = await flightsApiClient.get<AircraftWithSeats>(`${API_PATH}/${id}/seats`);
   return response.data;
 };
 
@@ -20,6 +25,7 @@ const update = async (id: number, data: Aircraft): Promise<Aircraft> => {
 
 export const aircraftService = {
   getAll,
+  getWithSeats,
   create,
   update,
 };
--- frontend/TheBlueSky.App/src/types/aircraft.ts
+++ frontend/TheBlueSky.App/src/types/aircraft.ts
@@ -1,8 +1,6 @@
-export const AIRCRAFT_MANUFACTURERS = [
-  'Unknown',
-  'Airbus',
-  'Boeing'
-] as const;
+import type { AircraftSeat } from "./aircraftSeat";
+
+export const AIRCRAFT_MANUFACTURERS = ['Boeing', 'Airbus'] as const;
 
 export type AircraftManufacturer = typeof AIRCRAFT_MANUFACTURERS[number];
 
@@ -20,3 +18,25 @@ export type Aircraft = {
 };
 
 export type NewAircraft = Omit<Aircraft, 'aircraftId' | 'ownerUserId'>;
+
+export type AircraftWithSeats = {
+  aircraft: Aircraft;
+  seats: AircraftSeat[];
+};
+
+export const AIRCRAFT_MODELS = [
+  'Boeing 737',
+  'Boeing 777',
+  'Boeing 787 Dreamliner',
+  'Airbus A320',
+  'Airbus A350',
+  'Airbus A380',
+] as const;
+
+export const MODELS_BY_MANUFACTURER: Record<AircraftManufacturer, string[]> = {
+  Boeing: ['Boeing 737', 'Boeing 777', 'Boeing 787 Dreamliner'],
+  Airbus: ['Airbus A320', 'Airbus A350', 'Airbus A380']
+};
+
+export const ALL_AIRCRAFT_MODELS = Object.values(MODELS_BY_MANUFACTURER).flat();
+export type AircraftModel = (typeof ALL_AIRCRAFT_MODELS)[number];
