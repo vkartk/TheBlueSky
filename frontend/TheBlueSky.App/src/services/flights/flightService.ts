d570419386c88c86d840c5ae3474ca38c55c7b2c
Karthik <53650724+vkartk@users.noreply.github.com>
2025-10-03 09:03:15 +0530

    feat: add Flight Service

---
+++ frontend/TheBlueSky.App/src/services/flights/flightService.ts
@@ -0,0 +1,19 @@
+import type { Flight } from '@/types/flight';
+import { flightsApiClient } from './flightsApiClient';
+
+const API_ENDPOINT = '/api/flight';
+
+const getAll = async (): Promise<Flight[]> => {
+    const response = await flightsApiClient.get<Flight[]>(API_ENDPOINT);
+    return response.data;
+};
+
+const update = async (flightId: number, data: Flight): Promise<void> => {
+    const response = await flightsApiClient.put(`${API_ENDPOINT}/${flightId}`, data);
+    return response.data;
+};
+
+export const flightService = {
+    getAll,
+    update,
+};
---
+++ frontend/TheBlueSky.App/src/types/flight.ts
@@ -0,0 +1,21 @@
+export const flightStatuses = [
+  'Scheduled',
+  'Boarding',
+  'Departed',
+  'Arrived',
+  'Delayed',
+  'Cancelled',
+  'Diverted',
+] as const;
+
+export type FlightStatus = (typeof flightStatuses)[number];
+
+export type Flight = {
+  flightId: number;
+  flightScheduleId: number;
+  flightDate: string; // YYYY-MM-DD
+  departureDateTime: string;
+  arrivalDateTime: string;
+  flightStatus: FlightStatus;
+  availableSeats: number;
+};
