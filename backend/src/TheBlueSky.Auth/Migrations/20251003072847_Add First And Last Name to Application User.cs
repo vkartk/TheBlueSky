afe2a6cf7f0dd58fe24fac0c1c37bafe25cde9de
Karthik <53650724+vkartk@users.noreply.github.com>
2025-10-03 13:01:12 +0530

    feat(migration): Add First & Last Name to Application User Migration

---
+++ backend/src/TheBlueSky.Auth/Migrations/20251003072847_Add First And Last Name to Application User.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheBlueSky.Auth.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFirstAndLastNametoApplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+        }
+    }
+}
