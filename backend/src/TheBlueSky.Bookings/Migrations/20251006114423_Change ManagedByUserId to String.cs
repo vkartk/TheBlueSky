baaf9fa97ca055c5574c6affbb5973e17802a09d
Karthik <53650724+vkartk@users.noreply.github.com>
2025-10-06 17:15:09 +0530

    feat(migration): Change ManagedByUserId to String

---
+++ backend/src/TheBlueSky.Bookings/Migrations/20251006114423_Change ManagedByUserId to String.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheBlueSky.Bookings.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeManagedByUserIdtoString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ManagedByUserId",
+                table: "Passengers",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ManagedByUserId",
+                table: "Passengers",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
