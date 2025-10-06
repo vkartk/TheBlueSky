0c78d80f35e06c6f99c034356acd8798e89274d0
Karthik <53650724+vkartk@users.noreply.github.com>
2025-10-06 12:36:29 +0530

    fix(mapping): Allow seat class updates

--- backend/src/TheBlueSky.Flights/Mappings/AircraftSeatProfile.cs
+++ backend/src/TheBlueSky.Flights/Mappings/AircraftSeatProfile.cs
@@ -13,15 +13,12 @@ namespace TheBlueSky.Flights.Mappings
 
             CreateMap<CreateAircraftSeatRequest, AircraftSeat>()
                 .ForMember(d => d.AircraftSeatId, opt => opt.Ignore())
-                .ForMember(d => d.IsActive, opt => opt.Ignore())
                 .ForMember(d => d.Aircraft, opt => opt.Ignore())
-                .ForMember(d => d.SeatClass, opt => opt.Ignore())
                 .ForMember(d => d.FlightSeatStatuses, opt => opt.Ignore()); 
             
             CreateMap<UpdateAircraftSeatRequest, AircraftSeat>()
                 .ForMember(d => d.AircraftId, opt => opt.Ignore())
                 .ForMember(d => d.Aircraft, opt => opt.Ignore())
-                .ForMember(d => d.SeatClass, opt => opt.Ignore())
                 .ForMember(d => d.FlightSeatStatuses, opt => opt.Ignore());
         }
 
