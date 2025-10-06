56de1f6e69d73e5062785531116924661bb5182f
Karthik <53650724+vkartk@users.noreply.github.com>
2025-10-06 09:11:04 +0530

    feat: add SeatClass enum

---
+++ backend/src/TheBlueSky.Flights/Enums/SeatClass.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TheBlueSky.Flights.Enums
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum SeatClass
+    {
+        Economy = 1,
+        Business = 2,
+        FirstClass = 3
+    }
+}
