e7da5403a4bc48b97c8253376c0950fb11f60cbb
Karthik <53650724+vkartk@users.noreply.github.com>
2025-10-07 18:27:57 +0530

    feat: add bookingsApiClient  to setupAxiosInterceptors

--- frontend/TheBlueSky.App/src/services/axiosSetup.ts
+++ frontend/TheBlueSky.App/src/services/axiosSetup.ts
@@ -1,5 +1,6 @@
 import { authApiClient } from '@/services/auth/authApiClient';
 import { flightsApiClient } from '@/services/flights/flightsApiClient';
+import { bookingsApiClient } from '@/services/bookings/bookingsApiClient';
 import { createAttachAuthTokenInterceptor, createHandleTokenRefreshInterceptor } from './interceptors';
 import type { AppStore } from '@/store';
 
@@ -7,7 +8,7 @@ export const setupAxiosInterceptors = (store: AppStore) => {
     const attachAuthToken = createAttachAuthTokenInterceptor(store);
     const handleTokenRefresh = createHandleTokenRefreshInterceptor(store);
 
-    const clients = [authApiClient, flightsApiClient];
+    const clients = [authApiClient, flightsApiClient, bookingsApiClient];
 
     clients.forEach(client => {
         client.interceptors.request.use(attachAuthToken);
