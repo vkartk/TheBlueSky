7710864dceba5ba089964b2ea200d8be4d8b3f98
Karthik <53650724+vkartk@users.noreply.github.com>
2025-10-07 21:35:44 +0530

    feat: add booking service

--- frontend/TheBlueSky.App/src/services/bookings/bookingService.ts
+++ frontend/TheBlueSky.App/src/services/bookings/bookingService.ts
@@ -1,13 +1,49 @@
 import { bookingsApiClient } from "./bookingsApiClient";
 import type { Booking, CreateBookingRequest } from "@/types/booking";
 
+const API_PATH = '/Booking';
 
 export const createBooking = async (bookingData: CreateBookingRequest): Promise<Booking> => {
   try {
-    const response = await bookingsApiClient.post<Booking>('/Booking', bookingData);
+    const response = await bookingsApiClient.post<Booking>(API_PATH, bookingData);
     return response.data;
   } catch (error) {
     console.error('Error creating booking:', error);
     throw new Error('Failed to create booking.');
   }
 };
+
+const getAll = async (): Promise<Booking[]> => {
+  const res = await bookingsApiClient.get<Booking[]>(API_PATH);
+  return res.data;
+};
+
+const getByUserId = async (userId: string): Promise<Booking[]> => {
+  const res = await bookingsApiClient.get<Booking[]>(`${API_PATH}/user/${userId}`);
+  return res.data;
+};
+
+const getById = async (bookingId: number): Promise<Booking> => {
+    const res = await bookingsApiClient.get<Booking>(`${API_PATH}/${bookingId}`);
+    return res.data;
+};
+
+const create = async (data: CreateBookingRequest): Promise<Booking> => {
+  const res = await bookingsApiClient.post<Booking>(API_PATH, data);
+  return res.data;
+};
+
+const update = async (id: number, data: Booking): Promise<Booking> => {
+  const res = await bookingsApiClient.put<Booking>(`${API_PATH}/${id}`, data);
+  return res.data;
+};
+
+
+export const bookingService = {
+  getAll,
+  getByUserId,
+  getById,
+  create,
+  update,
+};
+
