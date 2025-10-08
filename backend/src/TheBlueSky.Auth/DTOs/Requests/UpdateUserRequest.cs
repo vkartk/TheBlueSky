e3bd6ed19bd79547e341ad51c2aa054fc239a6e0
Karthik <53650724+vkartk@users.noreply.github.com>
2025-10-08 11:43:37 +0530

    feat: add UpdateUserRequest

---
+++ backend/src/TheBlueSky.Auth/DTOs/Requests/UpdateUserRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheBlueSky.Auth.DTOs.Requests
+{
+    public class UpdateUserRequest
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public IList<string> Roles { get; set; }
+    }
+}
