2a710d4721ac84909f7608f934630e6350397385
Karthik <53650724+vkartk@users.noreply.github.com>
2025-10-08 15:21:15 +0530

    feat: update Register logo

--- frontend/TheBlueSky.App/src/pages/auth/register/index.tsx
+++ frontend/TheBlueSky.App/src/pages/auth/register/index.tsx
@@ -1,11 +1,22 @@
 import { RegisterForm } from "@/components/auth/register-form"
+import { Plane } from "lucide-react"
+import { Link } from "react-router"
 
 export default function RegisterPage() {
     return (
         <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
             <div className="flex w-full max-w-sm flex-col gap-6">
                 <a href="#" className="flex items-center gap-2 self-center font-medium">
-                    <img src="brandkit/logo.png" alt="BlueSky logo" />
+                    <Link
+                    to="/"
+                    className="flex items-center gap-2 font-bold justify-center"
+                    aria-label="TheBlueSky home"
+                >
+                    <Plane className="h-12 w-12 text-blue-500" />
+                    <span className="text-3xl">
+                        The<span className="text-blue-500">BlueSky</span>
+                    </span>
+                </Link>
                 </a>
                 <RegisterForm />
             </div>
