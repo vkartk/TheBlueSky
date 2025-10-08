e3cdda02d1e08d961c3ce41394b70b947e5b2eec
Karthik <53650724+vkartk@users.noreply.github.com>
2025-10-08 15:20:27 +0530

    feat: update Login Brand logo

--- frontend/TheBlueSky.App/src/pages/auth/login/index.tsx
+++ frontend/TheBlueSky.App/src/pages/auth/login/index.tsx
@@ -1,12 +1,21 @@
+import { Plane } from "lucide-react"
+import { Link } from "react-router"
 import { LoginForm } from "@/components/auth/login-form"
 
 export default function LoginPage() {
     return (
         <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
             <div className="flex w-full max-w-sm flex-col gap-6">
-                <a href="#" className="flex items-center gap-2 self-center font-medium">
-                    <img src="brandkit/logo.png" alt="BlueSky logo" />
-                </a>
+                <Link
+                    to="/"
+                    className="flex items-center gap-2 font-bold justify-center"
+                    aria-label="TheBlueSky home"
+                >
+                    <Plane className="h-12 w-12 text-blue-500" />
+                    <span className="text-3xl">
+                        The<span className="text-blue-500">BlueSky</span>
+                    </span>
+                </Link>
                 <LoginForm />
             </div>
         </div>
